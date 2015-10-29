ec85660f0c566aa838207bd34725d1e0d32908bf
Matthew Marple <marplemr@gmail.com>
2015-10-28 18:47:48 -0700

    added json file and loaded into the redux state.

--- voting-server/index.js
+++ voting-server/index.js
@@ -3,3 +3,10 @@ import startServer from './src/server'
 
 export const store = makeStore();
 startServer(store); //starts the socket.io server AND listens to redux state store (redux will emit a state event )
+
+// sets redux store with value from json file and starts the voting process with NEXT action type
+store.dispatch({
+  type: 'SET_DOGBREEDS',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
