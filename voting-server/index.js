01cbafebe753e861cf73a7df04b0df2f451cb88d
Matthew Marple <marplemr@gmail.com>
2015-10-28 18:35:00 -0700

    integrated redux with socket.io. server emits state on any state change and also emits state whenever a client connects

--- voting-server/index.js
+++ voting-server/index.js
@@ -2,4 +2,4 @@ import makeStore from './src/store';
 import startServer from './src/server'
 
 export const store = makeStore();
-startServer(); //starts the socket.io server
+startServer(store); //starts the socket.io server AND listens to redux state store (redux will emit a state event )
--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,6 +1,17 @@
 import Server from 'socket.io';
 
 //creats socket.io server and regular HTTP server on port 8090
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  //subscribe to redux store and emit any state changes (the ENTIRE state is sent each time) via socket.io, which will then go to the client app
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  //whenever a client connection occurs, emit the entire state to the client app
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+  });
+
 }
